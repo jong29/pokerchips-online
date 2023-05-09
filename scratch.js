f70c1696cf02777f57b89e02d07434e31a53b4d0
user <jongwoop97@gmail.com>
2023-05-09 23:26:35 +0900

    course exercises

---
+++ scratch.js
@@ -0,0 +1,5 @@
+const t = [1, 2, 3]
+
+const m1 = t.map(value => value * 2);
+console.log(m1);
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Hello = (props) => {
+  console.log(props);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  );
+}
+
+const App = () => {
+  const name = "Peter";
+  const age = 10;
+  const friends = ["maya", "cookkoo"];
+
+  return (
+    <div>
+      <Hello name = {name} age = {age}/>
+      <p>{friends}</p>
     </div>
   );
 }
