fa97fbeceb79aa7fc42fe2646ad08a89a50b835b
user <jongwoop97@gmail.com>
2023-05-22 23:15:56 +0900

    start page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 
+const Button = (props) => {
+  return (
+      <div>
+          <button></button>
+      </div>
+  );
+}
 
 const App = () => {
+  const [players, setPlayers] = useState(1);
+
   return (
     <div>
       <h1>Welcome to Online Poker Chips!</h1>
       <p>How many players?</p>
+      <p>{players} players</p>
+      <Button handleClick = {() => setPlayers(players - 1)} text="-" />
+      <Button handleClick = {() => setPlayers(players + 1)} text="+" />
     </div>
+
   );
 }
 
--- src/funcs.js
+++
@@ -1,7 +0,0 @@
-// who wins
-let curentBlind = 0;
-let currentPot = 0;
-
-
-// tie breaker
-
