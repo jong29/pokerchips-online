9b686de617d59e4c680ae12fba94b49cbca4e6b5
user <jongwoop97@gmail.com>
2023-05-23 22:23:41 +0900

    implement start match when finish choosing number of players

--- src/App.js
+++ src/App.js
@@ -3,24 +3,42 @@ import { useState } from 'react';
 
 const Button = (props) => {
   return (
-      <div>
-          <button></button>
-      </div>
+      <button onClick={props.handleClick}>
+        {props.text}
+      </button>
   );
+}
+
+const Match = (props) => {
+  if (props.startMatch) {
+    return (
+      <div>
+        Match Begin!
+      </div>
+    )
+  }
 }
 
 const App = () => {
   const [players, setPlayers] = useState(1);
+  const [startMatch, setMatch] = useState(false);
 
   return (
     <div>
       <h1>Welcome to Online Poker Chips!</h1>
       <p>How many players?</p>
       <p>{players} players</p>
-      <Button handleClick = {() => setPlayers(players - 1)} text="-" />
+      <Button handleClick = {() => {
+        if (players === 0){
+          setPlayers(0)  
+        } else {
+          setPlayers(players - 1)} 
+        }
+      } text="-" />
       <Button handleClick = {() => setPlayers(players + 1)} text="+" />
+      <Button handleClick = {() => setMatch(true)} text="OK" />
+      <Match startMatch = {startMatch} />
     </div>
-
   );
 }
 
