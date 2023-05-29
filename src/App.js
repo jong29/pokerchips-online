934fc25915113c8b6cd8d334bd8a3c225eb9f2d5
user <jongwoop97@gmail.com>
2023-05-30 00:15:10 +0900

    displays Players without error

--- src/App.js
+++ src/App.js
@@ -7,6 +7,20 @@ import Match from './components/Match.component'
 const App = () => {
   const [numPlayers, setNumPlayers] = useState(2);
   const [startMatch, setMatch] = useState(false);
+  const [players, setPlayers] = useState([{id:1, name:'connor'}, {id:2, name:'mcgregor'}]);
+  
+  const handleStart = () => {
+    const playerArray = []
+    for (let i=0; i<numPlayers; i++) {
+      const newPlayer = {
+        id: players.length + 1,
+        name: `Player ${i}`
+      };
+      playerArray.push(newPlayer);
+    }
+    setPlayers(playerArray);
+    setMatch(true);
+  }
 
   return (
     <div>
@@ -15,14 +29,14 @@ const App = () => {
       <p>{numPlayers} players</p>
       <Button handleClick = {() => {
         if (numPlayers === 2){
-          setPlayers(2)  
+          setNumPlayers(2)  
         } else {
           setNumPlayers(numPlayers - 1)} 
         }
       } text="-" />
       <Button handleClick = {() => setNumPlayers(numPlayers + 1)} text="+" />
-      <Button handleClick = {() => setMatch(true)} text="OK" />
-      <Match startMatch={startMatch} numPlayers={numPlayers} />
+      <Button handleClick = {handleStart} text="OK" />
+      <Match startMatch={startMatch} players={players} />
     </div>
   );
 }
--- src/components/Match.component.js
+++ src/components/Match.component.js
@@ -1,17 +1,7 @@
 const Match = (props) => {
-  const [players, setPlayers] = useState([{id:1, name:'connor'}, {id:2, name:'mcgregor'}]);
+  const players = props.players;
 
   if (props.startMatch) {
-    const numPlayers = props.numPlayers;
-    const playerArray = []
-    for (let i=0; i<numPlayers; i++) {
-      const newPlayer = {
-        id: players.length + 1,
-        name: `Player ${i}`
-      };
-      playerArray.push(newPlayer);
-    }
-    setPlayers(playerArray);
 
     return (
       <div>
