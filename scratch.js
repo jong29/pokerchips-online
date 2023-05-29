1068cc95405e226f241e613c6d926a6ab6b59afc
user <jongwoop97@gmail.com>
2023-05-29 23:57:10 +0900

    display players after match starts

--- scratch.js
+++
@@ -1,5 +0,0 @@
-const t = [1, 2, 3]
-
-const m1 = t.map(value => value * 2);
-console.log(m1);
-
--- src/App.js
+++ src/App.js
@@ -10,34 +10,49 @@ const Button = (props) => {
 }
 
 const Match = (props) => {
+  const [players, setPlayers] = useState([{id:1, name:'connor'}, {id:2, name:'mcgregor'}]);
+
   if (props.startMatch) {
+    const numPlayers = props.numPlayers;
+    const playerArray = []
+    for (let i=0; i<numPlayers; i++) {
+      const newPlayer = {
+        id: players.length + 1,
+        name: `Player ${i}`
+      };
+      playerArray.push(newPlayer);
+    }
+    setPlayers(playerArray);
+
     return (
       <div>
-        Match Begin!
+        {players.map(player => 
+          <div key={player.id}>{player.name}</div>
+        )}
       </div>
     )
   }
 }
 
 const App = () => {
-  const [players, setPlayers] = useState(1);
+  const [numPlayers, setNumPlayers] = useState(2);
   const [startMatch, setMatch] = useState(false);
 
   return (
     <div>
       <h1>Welcome to Online Poker Chips!</h1>
       <p>How many players?</p>
-      <p>{players} players</p>
+      <p>{numPlayers} players</p>
       <Button handleClick = {() => {
-        if (players === 0){
-          setPlayers(0)  
+        if (numPlayers === 0){
+          setNumPlayers(0)  
         } else {
-          setPlayers(players - 1)} 
+          setNumPlayers(numPlayers - 1)} 
         }
       } text="-" />
-      <Button handleClick = {() => setPlayers(players + 1)} text="+" />
+      <Button handleClick = {() => setNumPlayers(numPlayers + 1)} text="+" />
       <Button handleClick = {() => setMatch(true)} text="OK" />
-      <Match startMatch = {startMatch} />
+      <Match startMatch={startMatch} numPlayers={numPlayers} />
     </div>
   );
 }
