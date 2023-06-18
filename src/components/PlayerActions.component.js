dd42c8ea439511e413ce0b4859fc105e6c90aa67
user <jongwoop97@gmail.com>
2023-06-18 14:35:22 +0900

    display all betting options

--- src/components/PlayerActions.component.js
+++ src/components/PlayerActions.component.js
@@ -1,18 +1,31 @@
 import { useState } from "react";
-import BetOptions from './BetOptions.component';
 
 const PlayerActions = (props) => {
 
-    const [showBet, setShowBet] = useState(false);
-
-    const showBetOptions = () => {
-        setShowBet(true);
-    }
+    const [bet, setBet] = useState(0);
 
     return(
         <div>
-            <button onClick={showBetOptions}>Bet</button>
-            {showBet ? <BetOptions /> : null}
+            <div>
+                {bet} <button>bet!</button>
+            </div>
+            <div>
+                <button>Pot</button>
+                <button>Chips</button>
+                <button>1/3</button>
+                <button>1/2</button>
+                <button>2/3</button>
+                <button>All-In!</button>
+            </div>
+            <div>
+                <button>+</button>
+                <button>-</button>
+                <button>$1</button>
+                <button>$5</button>
+                <button>$25</button>
+                <button>$100</button>
+                <button>Clear</button>
+            </div>
         </div>
     )
 };
